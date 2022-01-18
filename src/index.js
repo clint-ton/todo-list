decdca96129a5168f88ec0302393adf52c98deab
Clinton <incineratednewt@gmail.com>
2022-01-18 13:26:38 +1300

    DOMHandler to display tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 const taskFactory = (description) => {
   let completed = false;
 
@@ -15,6 +17,7 @@ const taskFactory = (description) => {
   };
   const toggleCompleted = () => {
     completed = !completed;
+    console.log(isCompleted());
   };
 
   return {
@@ -25,9 +28,79 @@ const taskFactory = (description) => {
   };
 };
 
-const DOMHandler = (() => {})();
+const taskListFactory = (name) => {
+  const tasks = [];
 
-const testTask = taskFactory("Go to the gym");
-console.log(testTask.getDescription());
-testTask.setDescription("Do some programming");
-console.log(testTask.getDescription());
+  const getName = () => {
+    return name;
+  };
+
+  const getTasks = () => {
+    return tasks;
+  };
+
+  const addTask = (description) => {
+    const newTask = taskFactory(description);
+    tasks.push(newTask);
+    DOMHandler.createTaskItem(newTask);
+  };
+
+  return {
+    getName,
+    getTasks,
+    addTask,
+  };
+};
+
+const DOMHandler = (() => {
+  const createTaskItem = (task) => {
+    const container = document.createElement("div");
+    container.classList.add("task");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("click", task.toggleCompleted);
+    const description = document.createElement("span");
+    description.textContent = task.getDescription();
+
+    container.appendChild(checkbox);
+    container.appendChild(description);
+
+    const content = document.querySelector("#content");
+
+    content.appendChild(container);
+  };
+
+  const createEntryBox = () => {
+    const container = document.createElement("div");
+    container.classList.add("entryBox");
+
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    button.classList.add("submit");
+
+    input.addEventListener("keyup", (event) => {
+      if (event.key == "Enter") {
+        addTask(input.value);
+        input.value = "";
+      }
+    });
+
+    button.addEventListener("click", () => {
+      addTask(input.value);
+      input.value = "";
+    });
+
+    container.appendChild(input);
+    container.appendChild(button);
+
+    const content = document.querySelector("#content");
+    content.appendChild(container);
+  };
+
+  return {
+    createTaskItem,
+    createEntryBox,
+  };
+})();
+
+const mainTasks = taskListFactory("main");
